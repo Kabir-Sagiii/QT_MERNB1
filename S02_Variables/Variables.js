60490e749168282344c21ff24a250adab1b694b7
Kabir <sagithedeveloper@gmail.com>
2024-05-29 11:16:42 +0530

    added the code

---
+++ S02_Variables/Variables.js
@@ -0,0 +1,26 @@
+//Declaration: process of creating a variable
+
+username;
+
+var cityName;
+
+let sid;
+
+//Assignment : Process of assigning the data to variable
+
+username = "Raj Verma";
+
+cityName = "Delhi";
+
+sid = 1000;
+
+//Initialization
+var empName = "Riya Sharma";
+
+let price = 100;
+
+const pin = 123;
+
+//Re-assignment
+
+price = 300;
