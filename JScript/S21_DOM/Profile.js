0b66b5a669522d9b577e962f997447ec21495367
Kabir <sagithedeveloper@gmail.com>
2024-07-09 15:02:41 +0530

    added the code

---
+++ JScript/S21_DOM/Profile.js
@@ -0,0 +1,10 @@
+function changeProfile() {
+  var imgDomElement = document.getElementById("profile");
+
+  var h2DomElement = document.getElementById("title");
+
+  imgDomElement.src =
+    "https://th.bing.com/th/id/OIP.vf6tqF3ZfAAvJsEqI5-ubgHaLH?rs=1&pid=ImgDetMain";
+
+  h2DomElement.textContent = "Riya Sharma";
+}
